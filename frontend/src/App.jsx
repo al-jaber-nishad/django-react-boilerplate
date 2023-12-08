6b83e9089f39acdd587f1442c264eebb6b8cbc57
aj_nishad <mailnishad02@gmail.com>
2023-12-08 23:53:14 +0600

    Update static variable to environmental

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,24 @@
 import React, {useState} from 'react';
+const apiUrl = process.env.BACKEND_IP;
 
 export function App(props) {
  const [text, setText]= useState('')
  
- const BASE_URL = "";
+const data = `${apiUrl}/get_data/`;
 
-
-//branch api
-const data = `http://192.168.0.22:8005/get_data/`;
-
-  fetch(`${data}`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('data', data)
-        setText(data.text)
-      })
-      .catch(() => {});
+fetch(`${data}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('data', data)
+      setText(data.text)
+    })
+    .catch(() => {});
 
 return (
     <div className='App'>
       <h2>{text || ''}</h2>
       <h2></h2>
-      <h2>Hello React.</h2>
+      <h2>Hello from React.</h2>
     </div>
   );
 }
