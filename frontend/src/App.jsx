0e231131feadf956e4b8f458c022363dcc58bfba
aj_nishad <mailnishad02@gmail.com>
2023-12-10 02:24:49 +0600

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
+require('dotenv').config()
 
 export function App(props) {
   const [text, setText]= useState('')
 
   const BASE_URL = "";
 
-  console.log(process.env)
+  console.log(process.env.REACT_APP_BACKEND_IP)
   const data = `http://127.0.0.1:8000/get_data/`;
 
   fetch(`${data}`)
@@ -20,7 +21,7 @@ export function App(props) {
       <div className='App'>
         <h2>{text || ''}</h2>
         <h2></h2>
-        <h2>Hello from React asd</h2>
+        <h2>Hello from React asad</h2>
       </div>
     );
 }
